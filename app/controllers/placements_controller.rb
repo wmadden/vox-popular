8a429d0ae7afa117626c294387a5f450cec82853
atroyn <anton.troynikov@gmail.com>
2013-01-25 23:13:38 +0100

    Added "new" to placements controller

--- app/controllers/placements_controller.rb
+++ app/controllers/placements_controller.rb
@@ -1,11 +1,12 @@
 class PlacementsController
 
+  def new
+    @placement = Placement.new
+  end
+
   def create
-
     placement = Playlist.find(params[:playlist_id]).placements.new(params[:placement])
-
     placement.save!
-
   end
 
 end
