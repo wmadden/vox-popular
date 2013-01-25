9f03d9618d3db5c376e5b0536b0a547d5e8e8a6d
Will Madden <w.a.madden@gmail.com>
2013-01-25 15:44:48 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeanBar::Application.config.secret_token = 'e25534d6416d6643bf9033acbdb429eb1758e4f7de1be67ad598a22ca893ddbc28cd88a7f9b48c8d082063f405c60c707391a82a71459772f2d84e8241f9bf56'
