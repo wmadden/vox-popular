61f2b8c6c204c5dc7b2d8e3cce44e99888fff74a
atroyn <anton.troynikov@gmail.com>
2013-01-26 10:52:39 +0100

    venue placement 'update'

--- app/controllers/venue/placements_controller.rb
+++ app/controllers/venue/placements_controller.rb
@@ -1,7 +1,10 @@
 class PlacementsController < ApplicationController
 
   def update
-    
+
+    @placement = Placement.find(params[:placement_id])
+    @placement.update_attributes(params[:placement])
+
   end
 
 end
