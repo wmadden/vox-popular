2a2919adfd45f43fba598635965e0395a1833393
Will Madden <w.a.madden@gmail.com>
2013-01-26 10:20:50 +0100

    Added new JS manifests for precompilation

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module LeanBar
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile += %w[patron.js public.js venue.js]
   end
 end
