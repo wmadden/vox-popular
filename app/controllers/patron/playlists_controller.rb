f036b76234ae858ec8f9fb0a125e071214356994
Will Madden <w.a.madden@gmail.com>
2013-01-26 19:32:37 +0100

    Fixed some bugs in the controllers and routes for voting

--- app/controllers/patron/playlists_controller.rb
+++ app/controllers/patron/playlists_controller.rb
@@ -1,7 +1,7 @@
 class Patron::PlaylistsController < PatronController
 
   def show
-    @playlist = Playlist.find(params[:playlist_id])
+    @playlist = Playlist.find(params[:id])
     @unvoted_placements = @playlist.placements.select do |pm|
       pm.votes.where(patron_id: @patron.id).count == 0
     end
