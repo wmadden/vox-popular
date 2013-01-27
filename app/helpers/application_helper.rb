6b3c60609411643e90d09714dccf99e3f2b42fe3
Will Madden <w.a.madden@gmail.com>
2013-01-27 14:17:30 +0100

    Refactored a bunch of placement views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def render_placement(placement, playlist)
+    {
+        soundcloud_track_id: placement.track.sc_track_id,
+        id:                  placement.id,
+        playlist_id:         playlist.id,
+        votes:               placement.votes
+    }
+  end
 end
